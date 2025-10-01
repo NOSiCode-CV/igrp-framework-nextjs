e0c8d2f5251785b3e0e490f6c94f56c675b91908
Elias Reis <elias.reis@gov.cv>
2025-10-01 16:33:41 -0100

    avatar updates

---
+++ packages/design-system-storybook/src/stories/avatar.stories.tsx
@@ -0,0 +1,307 @@
+import type { Meta, StoryFn, StoryObj } from '@storybook/nextjs-vite';
+import { IGRPAvatar, type IGRPAvatarProps ,IGRPIconObject} from '@igrp/igrp-framework-react-design-system';
+import { IGRPButtonPrimitive } from '@igrp/igrp-framework-react-design-system';
+
+const avatars = [
+  { src: "https://originui.com/avatar-80-04.jpg", fallback: "A" },
+  { src: "https://originui.com/avatar-80-05.jpg", fallback: "B" },
+  { src: "https://originui.com/avatar-80-06.jpg", fallback: "C" },
+  { src: "https://originui.com/avatar-80-07.jpg", fallback: "D" },
+  { src: "https://originui.com/avatar-80-08.jpg", fallback: "E" }
+];
+
+const maxVisible = 4;
+const visibleAvatars = avatars.slice(0, maxVisible);
+const remaining = avatars.length - maxVisible;
+
+export default {
+  title: 'Components/Avatar',
+  component: IGRPAvatar,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    src:{
+      control: 'text',
+      description: 'Specifies the path to the image file',
+    },
+    size:{
+      control: 'number',
+      description: 'Avatar size',
+    },
+    fallback: { 
+      control: 'text',
+      description: 'Simplified version of a user`s primary avatar when it cannot be shown ',
+     },
+    hasStatus: { 
+      control: 'boolean',
+      options: [true,false],
+      description: 'Avatar has State',
+      defaultValue: 'False',
+     },
+    status: {  
+      control: 'select',
+      description:'Avatar current state',
+      options: ['primary', 'success','destructive','warning','info','secondary','indigo'],
+    },
+    showIcon: { 
+      control: 'boolean',
+      options: [true,false],
+      description: 'If Avatar has Icon',
+      defaultValue: 'False',
+     },
+    className: { 
+      control: 'text',
+      description: 'If Avatar has Icon',
+     },
+    imageClassName: { control: 'text' },
+    iconName: {
+      control: 'select',
+      options: IGRPIconObject,
+      description: 'Select an icon',
+      defaultValue:'Circle'
+    },
+    iconSize : { control: 'number' },
+    iconPlacement: {
+      control: 'select',
+      options: ['start', 'end'],
+    },
+    iconClassName: { control: 'text' },
+
+    showBadge: { control: 'boolean' },
+    badgeNumber : { control: 'number' },
+    fallbackIcon : {
+      control: 'select',
+      options: IGRPIconObject,
+      description: 'Select an icon as Fallback',
+      defaultValue:'User'
+    },
+    hasFallbackIcon : { 
+      control: 'boolean',
+      options: [true,false],
+      description: 'If Avatar has Icon',
+      defaultValue: 'False',
+     },
+
+  },
+} as Meta;
+
+const Template: StoryFn<IGRPAvatarProps> = (args) => (
+  <div className='container my-10 mx-auto p-4 border rounded-lg shadow-sm'>
+    <IGRPAvatar {...args} />
+  </div>
+);
+
+export const Default: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://originui.com/avatar-80-04.jpg',
+    fallback:'Avatar Aang',
+    iconName:'Circle',
+    status:'success',
+  },
+};
+
+export const AvatarShape: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://originui.com/avatar-80-04.jpg',
+    fallback:'AV',
+    imageClassName:'rounded-md',
+    iconName:'Circle',
+    status:'success',
+  },
+};
+
+export const FallBackText: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    fallback:'AVasas',
+  },
+};
+
+export const FallBackIcon: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    hasFallbackIcon:true,
+    fallbackIcon:'User',
+  },
+};
+
+export const Status: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s',
+    hasStatus:true,
+    status:'success',
+    className:'overflow-visible',
+    imageClassName: 'rounded-full',
+  },
+};
+export const Size: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s',
+    className:'overflow-visible',
+    imageClassName: 'rounded-full',
+    size:120
+  },
+};
+
+export const HasIcon: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s',
+    showIcon:true,
+    iconName:'Apple',
+    className:'overflow-visible',
+  },
+};
+
+export const HasBadge: StoryObj<IGRPAvatarProps> = {
+  render: Template,
+  args: {
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s',
+    showBadge:true,
+    iconName:'Circle',
+    className:'overflow-visible',
+    badgeNumber :6
+  },
+  
+};
+
+type Story = StoryObj<typeof IGRPAvatar>;
+
+export const MultipleBadge: Story = {
+  
+  render: () => (
+
+      <div className="flex -space-x-3">
+        {visibleAvatars.map((avatar, index) => (
+          <IGRPAvatar
+            key={index}
+            src={avatar.src}
+            iconName="Circle"
+            className="overflow-visible ring-background ring-2"
+            imageClassName="rounded-full"
+            fallback={avatar.fallback}
+            hasStatus={false}
+            status="primary"
+            showIcon={false}
+            iconSize={0}
+            showBadge={false}
+            badgeNumber={0}
+            multiple={0}
+          />
+        ))}
+
+        {remaining > 0 && (
+            <IGRPButtonPrimitive
+            
+            className="bg-secondary text-muted-foreground ring-background hover:bg-secondary hover:text-foreground flex size-10 items-center justify-center rounded-full text-xs ring-2"
+            size="icon"
+          >
+            +3s
+          </IGRPButtonPrimitive>
+
+        )}
+          
+      </div>      
+  ),
+};
+
+export const MultipleBadgeWithText: Story = {
+  
+  render: () => (
+
+      <div className="flex -space-x-3 rounded-full border bg-gray-100">
+        {visibleAvatars.map((avatar, index) => (
+          <IGRPAvatar
+            key={index}
+            src={avatar.src}
+            iconName="Circle"
+            className="overflow-visible ring-background ring-2"
+            imageClassName="rounded-full"
+            fallback={avatar.fallback}
+            hasStatus={false}
+            status="primary"
+            showIcon={false}
+            iconSize={0}
+            showBadge={false}
+            badgeNumber={0}
+            multiple={0}
+          />
+        ))}
+
+        {remaining > 0 && (
+            <p
+            
+            className="flex items-center justify-center px-3"
+
+          >
+            Trusted by 60K+ developers.
+          </p>
+
+        )}
+          
+      </div>      
+  ),
+};
+
+
+// export const MultipleBadge2: Story = {
+  
+//   render: (args) => (
+//       <div className="flex -space-x-3">
+//        <IGRPAvatar 
+//             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s'
+//             iconName='Circle'
+//             className='overflow-visible ring-background rounded-full ring-1'
+//             imageClassName='rounded-full' 
+//             fallback=''
+//             hasStatus={false} 
+//             status={'primary'} 
+//             showIcon={false} 
+//             iconSize={0} 
+//             showBadge={false} 
+//             badgeNumber={args.badgeNumber}  
+
+//           />
+//           <IGRPAvatar 
+//             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s'
+//             iconName='Circle'
+//             className='overflow-visible ring-background rounded-full ring-1'
+//             imageClassName='rounded-full' 
+//             hasStatus={false} 
+//             status={'primary'} 
+//             showIcon={false} 
+//             iconSize={0} 
+//             showBadge={false} 
+//             badgeNumber={0} 
+
+//           />
+//           <IGRPAvatar 
+//             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s'
+//             iconName='Circle'
+//             className='overflow-visible ring-background rounded-full ring-1'
+//             imageClassName='rounded-full'  
+//             hasStatus={false} 
+//             status={'primary'} 
+//             showIcon={false} 
+//             iconSize={0} 
+//             showBadge={false} 
+//             badgeNumber={0} 
+//             multiple={0}
+//           />
+//             <IGRPButtonPrimitive
+//         variant="secondary"
+//         className="bg-secondary text-muted-foreground ring-background hover:bg-secondary hover:text-foreground flex size-10 items-center justify-center rounded-full text-xs ring-2"
+//         size="icon"
+//       >
+//         {remaining}
+//       </IGRPButtonPrimitive>
+//       </div>      
+//   ),
+  
+  
+// };
---
+++ packages/design-system/src/components/horizon/avatar.tsx
@@ -0,0 +1,112 @@
+// import { Badge } from 'lucide-react';
+import type { IGRPBaseAttributes } from '../../types';
+import { Avatar, AvatarImage, AvatarFallback } from '../primitives/avatar';
+import { IGRPColors, type IGRPColorVariants } from '../../lib/colors';
+import { cn } from '../../lib/utils';
+import { IGRPIcon } from './icon';
+import { Button } from '../primitives/button';
+
+interface IGRPAvatarProps extends IGRPBaseAttributes  {
+  src?: string;
+  size?:number;
+  alt?: string;
+  fallback?: string;
+  fallbackIcon?: string;
+  hasFallbackIcon?: boolean;
+  hasStatus?: boolean;
+  status: IGRPColorVariants;
+  showIcon?: boolean;
+  iconName?: string;
+  showBadge?: boolean;
+  className?: string,
+  imageClassName?: string,
+  badgeNumber?: number;
+  iconNumber?: number;
+  multiple?: number;
+}
+function IGRPAvatar({
+  src ="igrp",
+  fallback,
+  hasStatus = false,
+  showBadge = false,
+  status = 'primary',
+  showIcon,
+  iconName = 'Circle',
+  className,
+  iconPlacement = 'start',
+  iconClassName,
+  imageClassName,
+  badgeNumber =  6,
+  size = 32,
+  fallbackIcon = 'User',
+  hasFallbackIcon = false,
+  // multiple = 2
+}: IGRPAvatarProps) {
+  const colorClasses = IGRPColors['solid'][status];
+  const positionIcon = iconPlacement === 'start' ? 'start-0' : 'end-0 pe-3';
+  const upperFallBack = fallback ?  
+    fallback.split(" ")
+    .slice(0,2)                  
+    .map(word => word.charAt(0))
+    .join("")        
+    .toUpperCase() : " " ;
+ 
+  
+  //const positionParentIcon = iconPlacement === 'start' ? 'ps-9' : 'pe-9';
+  return (
+    <div className='flex items-center justify-center rounded-full p-1 w-10 h-10'>
+      <Avatar className={cn('overflow-visible', className)}  
+        style={{
+          height:size,
+          width:size,
+        }} >
+        <AvatarImage src={src} className={cn('rounded-full', imageClassName)} alt="avatar"  />
+        <AvatarFallback>
+          {hasFallbackIcon && (
+          <IGRPIcon iconName={fallbackIcon}   
+              style={{
+                height:size * 0.625,
+                width:size * 0.625,
+                top: `${size * 0.05}px`,   // 5% from top
+                left: `${size * 0.05}px`,  // 5% from left
+              }} 
+            />)}
+            {!hasFallbackIcon && upperFallBack}
+        </AvatarFallback>
+        {hasStatus && (
+          <span
+            style={{
+              height:size * 0.375,
+              width: size * 0.375,
+            }} 
+            className={cn(
+              'border-background absolute -end-0.5 -bottom-0.5 size-3 rounded-full border-2 bg-emerald-500',
+              colorClasses.bgForeground ? colorClasses.bgForeground : '',
+            )}
+          >
+            <span className="sr-only"></span>
+          </span>
+        )}
+        {showIcon && (
+          <div className={cn('absolute -top-2 left-5  rounded-full border-2 bg-white', positionIcon)}>
+          <IGRPIcon iconName={iconName} className={iconClassName}  
+              style={{
+                height:size * 0.625,
+                width:size * 0.625,
+                top: `${size * 0.05}px`,   // 5% from top
+                left: `${size * 0.05}px`,  // 5% from left
+              }} 
+            />
+          </div>
+        )}
+        {showBadge && (
+          <div className={cn('inline-flex items-center justify-center rounded-full border text-xs font-medium w-fit whitespace-nowrap shrink-0 gap-1 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] transition-[color,box-shadow] leading-normal bg-primary text-primary-foreground [a&]:hover:bg-primary/90 border-background absolute -top-2 left-full min-w-5 -translate-x-3 px-1', positionIcon)}>
+            {badgeNumber}
+           {/* <span className={cn('absolute -top-0.5 left-1 text-sm', positionIcon)}>{badgeNumber}</span>  */}
+          </div>
+        )}
+      </Avatar>
+    </div>
+  );
+}
+export { IGRPAvatar, type IGRPAvatarProps ,Button};

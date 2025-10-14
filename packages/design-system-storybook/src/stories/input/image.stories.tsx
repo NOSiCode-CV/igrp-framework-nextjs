40c08a6a4659e2d8aaee9a757dc12e06d218fe43
Elias Reis <elias.reis@gov.cv>
2025-10-14 16:57:45 -0100

    update image, border-radius, width, height & ratio

--- packages/design-system-storybook/src/stories/input/image.stories.tsx
+++ packages/design-system-storybook/src/stories/input/image.stories.tsx
@@ -10,6 +10,18 @@ export default {
     width: { control: 'number' },
     height: { control: 'number' },
     labelClassName: { control: 'text' },
+    ratio: { control: 'select',
+      options: ['1/1', '4/3', '16/9', '21/9'],
+      description: 'Image ratio',
+      default: '16/9',
+     },
+     borderRadius: {
+      control: 'select',
+      description: 'Radius',
+      options: ['rounded-none', 'rounded-sm', 'rounded', 'rounded-md', 'rounded-lg', 'rounded-xl', 'rounded-2xl', 'rounded-3xl', 'rounded-full'],
+      defaultValue: 'rounded-none',
+    },
+
   },
 } as Meta;
 
@@ -23,7 +35,6 @@ export const Default: StoryObj<IGRPImageProps> = {
   render: Template,
   args: {
     src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf_HNkqoUPKhNV0qukwpQFXeBap9onZrkNRA&s',
-    width:500,
-    height:500
+    ratio:1/1,
   },
 };
--- packages/design-system/src/components/horizon/image/index.tsx
+++ packages/design-system/src/components/horizon/image/index.tsx
@@ -1,25 +1,35 @@
-
-
+import  { AspectRatio} from '@radix-ui/react-aspect-ratio';
 import type { IGRPImageAttributes } from '../../../types'
 import Image from 'next/image'
+import { cn } from '../../../lib/utils';
+
 interface IGRPImageProps extends IGRPImageAttributes {
     src: string; 
     alt: string;
-    width: number;
-    height: number;
     labelClassName:string;
+    ratio:number;
+    borderRadius?: 'rounded-none' | 'rounded-sm' | 'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-xl' | 'rounded-2xl' | 'rounded-3xl' | 'rounded-full';
 }
 
 function IGRPImage({
     src,
-    width,
-    height,
     alt,
     labelClassName,
+    ratio,
+    borderRadius
 
 }:IGRPImageProps){
+    const ratioNumber = {
+        '1/1':1/1,
+        '4/3':4/3,
+        '16/9':16/9 ,
+        '21/9':21/9,
+      }[ratio]
+
 return(
-        <Image src={src} alt={alt} width={width} height={height} className={labelClassName}/>
+    <AspectRatio ratio={ratioNumber}>
+        <Image src={src} alt={alt} fill className={cn('h-full w-full object-cover',borderRadius ,labelClassName)}/>
+    </AspectRatio>
 )
 }
 export { IGRPImage,type IGRPImageProps }

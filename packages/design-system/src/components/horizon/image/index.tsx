5b3e79356e639be6fc385845a516748448a15118
Elias Reis <elias.reis@gov.cv>
2025-10-20 15:07:09 -0100

    improves

--- packages/design-system/src/components/horizon/image/index.tsx
+++ packages/design-system/src/components/horizon/image/index.tsx
@@ -1,65 +1,48 @@
-import  { AspectRatio} from '@radix-ui/react-aspect-ratio';
-import type { IGRPImageAttributes } from '../../../types'
-import Image from 'next/image'
+import Image from 'next/image';
 import { cn } from '../../../lib/utils';
+import type { IGRPImageAttributes, IGRPRoundSize } from '../../../types';
+import { AspectRatio } from '../../primitives/aspect-ratio';
 
 interface IGRPImageProps extends IGRPImageAttributes {
-    src: string; 
-    alt: string;
-    labelClassName: string;
-    ratio: number;
-    width: number;
-    height: number;
-    borderRadius?: 'rounded-none' | 'rounded-sm' | 'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-xl' | 'rounded-2xl' | 'rounded-3xl' | 'rounded-full';
+  src: string;
+  alt: string;
+  className: string;
+  ratio: number;
+  width: number;
+  height: number;
+  borderRadius?: IGRPRoundSize;
+
 }
 
-function IGRPImage({
-    src,
-    alt,
-    labelClassName,
-    ratio,
-    borderRadius,
-    width,
-    height
+function IGRPImage({ src, alt,ratio, className, borderRadius, width, height }: IGRPImageProps) {
+		
+	const ratioNumber = {
+		'1/1':1/1,
+		'4/3':4/3,
+		'16/9':16/9 ,
+		'21/9':21/9,
+	}[ratio]
+  var useFill = !width;
 
-}:IGRPImageProps){
-    const ratioNumber = {
-        '1/1':1/1,
-        '4/3':4/3,
-        '16/9':16/9 ,
-        '21/9':21/9,
-      }[ratio]
+	const imageClasses = cn('object-cover object-center', borderRadius, className);
+  const Img = useFill ? (
+    <Image
+      src={src}
+      alt={alt}
+      fill
+      className={imageClasses}
+    />
+  ) : (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={imageClasses}
+    />
+  );
 
-    var useFill=!width
-
-    const Img = useFill ? (
-        <Image
-          src={src}
-          alt={alt}
-          fill
-          className={cn('object-cover object-center', borderRadius, labelClassName)}
-        />
-      ) : (
-        <Image
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          className={cn('object-cover object-center', borderRadius, labelClassName)}
-        />
-      );
-
-return(
-    <div className='flex justify-center'> 
-        {ratio ? 
-            (<AspectRatio ratio={ratioNumber}>
-                {Img}
-            </AspectRatio>
-            ):(
-                Img
-            ) 
-        }       
-    </div>
-)
+  return <div className="flex justify-center">
+		 <AspectRatio ratio={ratioNumber}>{Img}</AspectRatio></div>;
 }
-export { IGRPImage,type IGRPImageProps }
+export { IGRPImage, type IGRPImageProps };
--- packages/design-system/src/types.ts
+++ packages/design-system/src/types.ts
@@ -46,3 +46,15 @@ export type IGRPImageAttributes = {
 };
 
 export type IGRPGridSize = keyof typeof igrpGridSizeClasses;
+
+export type IGRPRoundSize = 
+  'rounded-none'|
+   'rounded-sm'|
+   'rounded'|
+   'rounded-md'|
+   'rounded-lg'|
+   'rounded-xl'|
+   'rounded-2xl'|
+   'rounded-3xl'|
+   'rounded-full'
+;

1fe3a008c233fda6098b26e6415c859050c1f331
Fidel da Luz <fidel.luz@nosi.cv>
2026-05-07 16:15:46 -0100

    feat(framework-next): add useLayoutData client hook

---
+++ packages/framework/next/src/client/use-layout-data.ts
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import {
+  revalidateAppsAction,
+  revalidateMenusAction,
+  revalidateUserAction,
+} from '../actions/index';
+
+export function useLayoutData(appCode: string) {
+  const router = useRouter();
+
+  return {
+    refreshMenus: async () => {
+      await revalidateMenusAction(appCode);
+      router.refresh();
+    },
+    refreshApps: async () => {
+      await revalidateAppsAction();
+      router.refresh();
+    },
+    refreshUser: async () => {
+      await revalidateUserAction();
+      router.refresh();
+    },
+  };
+}

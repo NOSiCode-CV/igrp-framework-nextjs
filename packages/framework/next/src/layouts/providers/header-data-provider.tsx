f37ea2a8b2f76ce4a98196066c769b0764463418
Fidel da Luz <fidel.luz@nosi.cv>
2026-05-07 16:23:23 -0100

    feat(framework-next): add HeaderDataProvider async server component

---
+++ packages/framework/next/src/layouts/providers/header-data-provider.tsx
@@ -0,0 +1,29 @@
+// packages/framework/next/src/layouts/providers/header-data-provider.tsx
+import type { IGRPConfigArgs } from '@igrp/framework-next-types';
+import { IGRPTemplateHeader } from '@igrp/framework-next-ui';
+
+import { fetchCurrentUser } from '../../hooks/use-user';
+
+type HeaderDataProviderProps = {
+  config: Pick<IGRPConfigArgs, 'layoutMockData' | 'previewMode'>;
+};
+
+export async function HeaderDataProvider({ config }: HeaderDataProviderProps) {
+  const { previewMode, layoutMockData } = config;
+  const headerData = await layoutMockData.getHeaderData();
+
+  if (previewMode) {
+    return <IGRPTemplateHeader data={headerData} />;
+  }
+
+  const [user] = await Promise.all([fetchCurrentUser()]);
+
+  return (
+    <IGRPTemplateHeader
+      data={{
+        ...headerData,
+        ...(user !== null && { user }),
+      }}
+    />
+  );
+}

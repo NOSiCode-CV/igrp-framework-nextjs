6f52fb9370ea1c18add4244c33f771f6474cc387
Fidel da Luz <fidel.luz@nosi.cv>
2026-05-18 19:36:46 -0100

    fix(next-ui): remove unused session dependency from session-watcher effect

--- packages/framework/next-ui/src/components/templates/session-watcher.tsx
+++ packages/framework/next-ui/src/components/templates/session-watcher.tsx
@@ -5,13 +5,13 @@ import { useRouter } from 'next/navigation';
 import { useSession } from '@igrp/framework-next-auth/client';
 
 export function IGRPSessionWatcher({ children }: { children: React.ReactNode }) {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   const router = useRouter();
   useEffect(() => {
     if (status === 'unauthenticated') {
       router.push('/login');
     }
-  }, [status, session, router]);
+  }, [status, router]);
 
   return children;
 }
